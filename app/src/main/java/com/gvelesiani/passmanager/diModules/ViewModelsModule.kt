ab24378c3455ebc28218ec1909c291fe7a1f8bb6
Bacho Gvelesiani <agvelesiani@tbcbank.com.ge>
2022-02-20 15:58:06 +0400

    initial structure setup

--- app/src/main/java/com/gvelesiani/passmanager/diModules/ViewModelsModule.kt
+++ app/src/main/java/com/gvelesiani/passmanager/diModules/ViewModelsModule.kt
@@ -1,11 +1,23 @@
 package com.gvelesiani.passmanager.diModules
 
+import com.gvelesiani.passmanager.ui.addPassword.AddPasswordViewModel
+import com.gvelesiani.passmanager.ui.checkPassword.CheckPasswordViewModel
 import com.gvelesiani.passmanager.ui.home.HomeViewModel
+import com.gvelesiani.passmanager.ui.passwords.PasswordsViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val viewModelsModule = module {
     viewModel {
         HomeViewModel()
+    }
+    viewModel {
+        AddPasswordViewModel()
+    }
+    viewModel {
+        CheckPasswordViewModel()
+    }
+    viewModel {
+        PasswordsViewModel()
     }
 }
